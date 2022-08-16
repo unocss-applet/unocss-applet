fb1cfbd53913d64311e8e30600ca0a8864b2b5d1
Neil <zguolee@foxmail.com>
2022-08-16 17:08:00 +0800

    fix: playground not effect

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,8 +1,11 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+
+import 'uno.css'
+
 export function createApp() {
-  const app = createSSRApp(App);
+  const app = createSSRApp(App)
   return {
     app,
-  };
+  }
 }
