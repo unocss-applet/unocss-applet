6a347f07d6854754803c12f21a9d16a2a897f104
Neil Lee <zguolee@foxmail.com>
2023-10-19 11:09:10 +0800

    test: update

--- test/preset-rem-rpx.test.ts
+++ test/preset-rem-rpx.test.ts
@@ -28,30 +28,39 @@ describe('rem-rpx', () => {
 
   it('should rem to rpx', async () => {
     expect((await unoRemToRpx.generate(
-      new Set(['m4', 'mx2', '-p2', 'gap2', 'space-x-4']),
+      new Set(['m4', 'm--4', 'mx2', '-p2', 'gap2', 'space-x-4', 'space-x--4', '-space-x-4', 'divide-x-4', 'divide-x-1rem']),
       { preflights: false },
     )).css)
       .toMatchInlineSnapshot(`
         "/* layer: default */
+        .m--4{margin:-32rpx;}
         .m4{margin:32rpx;}
         .mx2{margin-left:16rpx;margin-right:16rpx;}
         .gap2{gap:16rpx;}
+        .-space-x-4>:not([hidden])~:not([hidden]){--un-space-x-reverse:0;margin-left:calc(calc(32rpx * calc(1 - var(--un-space-x-reverse))) * -1);margin-right:calc(calc(32rpx * var(--un-space-x-reverse)) * -1);}
+        .space-x--4>:not([hidden])~:not([hidden]){--un-space-x-reverse:0;margin-left:calc(-32rpx * calc(1 - var(--un-space-x-reverse)));margin-right:calc(-32rpx * var(--un-space-x-reverse));}
         .space-x-4>:not([hidden])~:not([hidden]){--un-space-x-reverse:0;margin-left:calc(32rpx * calc(1 - var(--un-space-x-reverse)));margin-right:calc(32rpx * var(--un-space-x-reverse));}
+        .divide-x-1rem>:not([hidden])~:not([hidden]){--un-divide-x-reverse:0;border-left-width:calc(32rpx * calc(1 - var(--un-divide-x-reverse)));border-right-width:calc(32rpx * var(--un-divide-x-reverse));}
+        .divide-x-4>:not([hidden])~:not([hidden]){--un-divide-x-reverse:0;border-left-width:calc(4px * calc(1 - var(--un-divide-x-reverse)));border-right-width:calc(4px * var(--un-divide-x-reverse));}
         .-p2{padding:-16rpx;}"
       `)
   })
 
   it('should rpx to rem', async () => {
     expect((await unoRpxToRem.generate(
-      new Set(['m-32rpx', 'mx-16rpx', '-p-2prx', 'gap-2rpx', 'space-x-32rpx']),
+      new Set(['m-32rpx', 'm--32rpx', 'mx-16rpx', '-p-2prx', 'gap-2rpx', 'space-x-32rpx', 'space-x--32rpx', '-space-x-32rpx', 'divide-x-32rpx']),
       { preflights: false },
     )).css)
       .toMatchInlineSnapshot(`
         "/* layer: default */
+        .m--32rpx{margin:-1rem;}
         .m-32rpx{margin:1rem;}
         .mx-16rpx{margin-left:0.5rem;margin-right:0.5rem;}
         .gap-2rpx{gap:0.0625rem;}
-        .space-x-32rpx>:not([hidden])~:not([hidden]){--un-space-x-reverse:0;margin-left:calc(1rem * calc(1 - var(--un-space-x-reverse)));margin-right:calc(1rem * var(--un-space-x-reverse));}"
+        .-space-x-32rpx>:not([hidden])~:not([hidden]){--un-space-x-reverse:0;margin-left:calc(calc(1rem * calc(1 - var(--un-space-x-reverse))) * -1);margin-right:calc(calc(1rem * var(--un-space-x-reverse)) * -1);}
+        .space-x--32rpx>:not([hidden])~:not([hidden]){--un-space-x-reverse:0;margin-left:calc(-1rem * calc(1 - var(--un-space-x-reverse)));margin-right:calc(-1rem * var(--un-space-x-reverse));}
+        .space-x-32rpx>:not([hidden])~:not([hidden]){--un-space-x-reverse:0;margin-left:calc(1rem * calc(1 - var(--un-space-x-reverse)));margin-right:calc(1rem * var(--un-space-x-reverse));}
+        .divide-x-32rpx>:not([hidden])~:not([hidden]){--un-divide-x-reverse:0;border-left-width:calc(1rem * calc(1 - var(--un-divide-x-reverse)));border-right-width:calc(1rem * var(--un-divide-x-reverse));}"
       `)
   })
 })
