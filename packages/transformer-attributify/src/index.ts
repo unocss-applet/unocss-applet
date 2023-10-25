ccc6f5514bf4ba530537a2251382696354746e32
Neil <zguolee@foxmail.com>
2023-10-25 23:48:16 +0800

    fix: pseudo class for attributify (#42)

--- packages/transformer-attributify/src/index.ts
+++ packages/transformer-attributify/src/index.ts
@@ -16,7 +16,7 @@ export function transformerAttributify(options: TransformerAttributifyOptions = 
   const nonValuedAttribute = options?.nonValuedAttribute ?? true
   const prefix = options.prefix ?? 'un-'
   const prefixedOnly = options.prefixedOnly ?? false
-  const deleteAttributes = options.deleteClass ?? true
+  const deleteAttributes = options.deleteAttributes ?? true
 
   return {
     name: 'transformer-attributify',
@@ -68,25 +68,31 @@ export function transformerAttributify(options: TransformerAttributifyOptions = 
                 if (prefixedOnly && prefix && !name.startsWith(prefix))
                   continue
 
-                const attrs = await Promise.all(content.split(splitterRE).filter(Boolean)
-                  .map(async (v) => {
-                    let token = v
-                    // b="~ green dark:(red 2)"
-                    if (v === '~') {
-                      token = nonPrefixed
-                    }
-                    else if (v.includes(':')) {
-                      const splitV = v.split(':')
-                      token = `${splitV[0]}:${splitV[1]}`
-                    }
-                    else if (v.startsWith('!')) {
-                      token = `!${nonPrefixed}-${v.slice(1)}`
-                    }
-                    else { token = `${nonPrefixed}-${v}` }
+                const result: string[] = []
 
-                    return [token, !!await uno.parseToken(token)] as const
-                  }))
-                const result = attrs.filter(([, v]) => v).map(([v]) => v)
+                for (const v of content.split(splitterRE).filter(Boolean)) {
+                  let token = v
+                  // b="~ green dark:red dark:2"
+                  if (v.includes(':')) {
+                    const splitV = v.split(':')
+                    token = `${splitV[0]}:${splitV[1]}`
+                    if (await uno.parseToken(`${nonPrefixed}-${splitV[1]}`))
+                      result.push(`${splitV[0]}:${nonPrefixed}-${splitV[1]}`)
+                    else if (await uno.parseToken(`${splitV[0]}:${splitV[1]}`))
+                      result.push(`${splitV[0]}:${splitV[1]}`)
+                  }
+                  else {
+                    if (v === '~')
+                      token = nonPrefixed
+                    else if (v.startsWith('!'))
+                      token = `!${nonPrefixed}-${v.slice(1)}`
+                    else
+                      token = `${nonPrefixed}-${v}`
+                    if (await uno.parseToken(token))
+                      result.push(token)
+                  }
+                }
+
                 attrSelectors.push(...result)
                 result.length && deleteAttributes && (matchStrTemp = matchStrTemp.replace(` ${matchStr}`, ''))
               }
